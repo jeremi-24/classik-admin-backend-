efa12187d451a8a0719d65dd14994bf0dda4aa41
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-06-17 09:49:19 +0000

    modifier cors sur amel

--- src/main/java/skool/saas/skool/GLOBALE/config/WebConfig.java
+++ src/main/java/skool/saas/skool/GLOBALE/config/WebConfig.java
@@ -1,6 +1,8 @@
 package skool.saas.skool.GLOBALE.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
@@ -10,12 +12,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://classik-admin.netlify.app", "https://classik-admin-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+    @Autowired
+    private AnneeContextInterceptor anneeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(anneeInterceptor);
+    }
 
 }
-
-
