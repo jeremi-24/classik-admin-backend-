996bae0839072fe87ce2c96618b139dee2cfb182
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-06-17 09:59:31 +0000

    changement de cors sur back

--- src/main/java/skool/saas/skool/GLOBALE/config/WebConfig.java
+++ src/main/java/skool/saas/skool/GLOBALE/config/WebConfig.java
@@ -1,8 +1,6 @@
 package skool.saas.skool.GLOBALE.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
@@ -12,17 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://classik-admin.netlify.app", "https://classik-admin-production.up.railway.app")
+                .allowedOrigins("http://localhost:3000" , "https://classik-admin-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-    @Autowired
-    private AnneeContextInterceptor anneeInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(anneeInterceptor);
-    }
 
 }
+
+
